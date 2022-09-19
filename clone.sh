4bb55c617380251a7d24db63209e0d579703cff0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-09-19 16:28:36 +0900

    clone script

---
+++ clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone --mirror git://git.yoctoproject.org/meta-raspberrypi
+git clone --mirror git://git.yoctoproject.org/meta-security.git
+git clone --mirror git://git.yoctoproject.org/meta-virtualization
+git clone --mirror git://git.yoctoproject.org/poky
+git clone --mirror https://git.openembedded.org/meta-openembedded
+git clone --mirror https://git.openembedded.org/meta-python2
+git clone --mirror https://git.yoctoproject.org/git/meta-freescale
